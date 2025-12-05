b86d02e5ae403fd0f43a03860aabca1477663c40
deorbil <darwinbillian@outlook.com>
2025-12-05 08:23:34 +0700

    feat: enable zsh

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,8 @@
 
   # programs.firefox.enable = true;
 
+  programs.zsh.enable = true;
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   # environment.systemPackages = with pkgs; [
