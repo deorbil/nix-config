091ca00703d839df60137b1a6259db8242360c26
deorbil <darwinbillian@outlook.com>
2025-12-05 08:05:27 +0700

    feat: add user

--- configuration.nix
+++ configuration.nix
@@ -62,13 +62,10 @@
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
+  users.users.deorbil = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
 
   # programs.firefox.enable = true;
 
