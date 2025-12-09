190edfcba74d4cb63682ae44c39a065c06afd4cc
deorbil <darwinbillian@outlook.com>
2025-12-10 06:45:52 +0700

    feat: disable xserver

--- configuration.nix
+++ configuration.nix
@@ -32,9 +32,6 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
